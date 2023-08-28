35d83937e2614e3e6276ea956ae1795cc8500708
AVINASH KUMAR <Aviproo>
2023-08-28 15:59:15 +0530

    second commit

--- app.js
+++ app.js
@@ -1,1 +1,8 @@
-console.log("I love git");
+let header = document.getElementById("header-title");
+let mainheader = document.getElementById("main-header");
+let title = document.querySelector(".title");
+document.title = "sharpener";
+header.textContent = "Hello world";
+mainheader.style.border = "3px solid black";
+title.style.fontStyle = "bold";
+title.style.color = "green";
