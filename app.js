32af5b15b0228f902d17046bdad8399d886f9f1a
AVINASH KUMAR <Aviproo>
2023-08-29 12:29:38 +0530

    addes new li

--- app.js
+++ app.js
@@ -7,8 +7,9 @@ mainheader.style.border = "3px solid black";
 title.style.fontStyle = "bold";
 title.style.color = "green";
 let items = document.getElementsByClassName("list-group-item");
-
 for (let i = 0; i < items.length; i++) {
   items[i].style.fontWeight = "bold";
   items[i].style.backgroundColor = "green";
 }
+let added = document.getElementsByClassName("added");
+added.innerHtml = "New added Item";
